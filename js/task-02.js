9382c24525ff75929a46f31e9c4db4fce097814f
V-Yehor <es.valkoff@gmail.com>
2023-08-27 16:00:56 +0300

    Update task-02.js
    виконано завдання №2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector('#ingredients');
+// console.log(list); 
+const li = document.createElement('li');
+
+ingredients.forEach(element => {
+  const li = document.createElement('li');
+  li.classList.add('item');
+  li.textContent = element;
+  list.append(li);
+  // console.log(li);
+});
