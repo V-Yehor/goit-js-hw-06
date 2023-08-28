752fd31fcd7cf5aa3c96d17dbae20fa84ad65c6d
V-Yehor <es.valkoff@gmail.com>
2023-08-28 20:39:01 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeBtn = document.querySelector('.change-color');
+const color = document.querySelector('.color');
+const body = document.querySelector('body');
+
+const changeColor = () => {
+  color.textContent = getRandomHexColor();
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+changeBtn.addEventListener("click", changeColor);
