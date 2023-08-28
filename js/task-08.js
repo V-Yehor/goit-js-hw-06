03c5a9f7459b9308fd8487eef2bfdb0bc1fa852b
V-Yehor <es.valkoff@gmail.com>
2023-08-28 20:00:00 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const loginForm = document.querySelector(".login-form");
+const user = {};
 
+loginForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const userEmail = form.elements.email.value;
+    const userPass = form.elements.password.value;
+
+    if (userEmail === "" || userPass === "") {
+        alert('Всі поля повинні бути заповнені');
+        return;
+    } else {
+        user.email = userEmail;
+        user.password = userPass;
+    }
+    console.log(user);
+    form.reset();
+}
